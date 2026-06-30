54ef65ad3a42a2fb7099792dcce4bb3dd2021477
Narendra568 <sannidhi.narendra@mphasis.com>
2026-06-30 13:05:01 +0530

    Delete transaction-service directory

--- transaction-service/src/main/java/com/example/transactionservice/repository/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.transactionservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.transactionservice.model.TransactionRecord;
-
-public interface TransactionRepository extends JpaRepository<TransactionRecord, Long> {
-}
